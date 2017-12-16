29b36af7af280308f7cfc2ee7ffa1738085e0306
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-12-16 12:11:06 +0900

    Release 1.1.0

--- lib/appium_lib_core/version.rb
+++ lib/appium_lib_core/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Core
-    VERSION = '1.0.0'.freeze unless defined? ::Appium::Core::VERSION
-    DATE    = '2017-11-12'.freeze unless defined? ::Appium::Core::DATE
+    VERSION = '1.1.0'.freeze unless defined? ::Appium::Core::VERSION
+    DATE    = '2017-12-16'.freeze unless defined? ::Appium::Core::DATE
   end
 end
