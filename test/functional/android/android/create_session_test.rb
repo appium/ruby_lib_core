991f8ac0cc8e084307f64f2ce01ae43a32b66502
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-01-02 00:36:22 +0900

    fix tests for create session

--- test/functional/android/android/create_session_test.rb
+++ test/functional/android/android/create_session_test.rb
@@ -5,24 +5,42 @@ class AppiumLibCoreTest
   module WebDriver
     class CreateSessionTestTest < AppiumLibCoreTest::Function::TestCase
       def test_mjsonwp
-        caps = Caps::ANDROID_OPS
-        caps[:caps][:forceMjsonwp] = false
-        core = ::Appium::Core.for(self, caps)
+        caps = Caps::ANDROID_OPS[:caps].merge({forceMjsonwp: true})
+        new_caps = Caps::ANDROID_OPS.merge({caps: caps})
+        core = ::Appium::Core.for(self, new_caps)
 
         driver = core.start_driver
 
         assert_equal :oss, driver.dialect
+        assert driver.capabilities[:forceMjsonwp].nil?
+        assert driver.capabilities['forceMjsonwp'].nil?
       end
 
       # Require Appium 1.7.2+
       def test_w3c
+        caps = Caps::ANDROID_OPS[:caps].merge({forceMjsonwp: false})
+        new_caps = Caps::ANDROID_OPS.merge({caps: caps})
+        core = ::Appium::Core.for(self, new_caps)
+
+        driver = core.start_driver
+
+        assert_equal :w3c, driver.dialect
+        assert driver.capabilities[:forceMjsonwp].nil?
+        assert driver.capabilities['forceMjsonwp'].nil?
+      end
+
+      # Require Appium 1.7.2+
+      def test_w3c_default
         caps = Caps::ANDROID_OPS
-        caps[:caps][:forceMjsonwp] = true
         core = ::Appium::Core.for(self, caps)
 
         driver = core.start_driver
 
         assert_equal :w3c, driver.dialect
+        assert driver.capabilities[:forceMjsonwp].nil?
+        assert driver.capabilities['forceMjsonwp'].nil?
+
+        driver.quit
       end
     end
   end
