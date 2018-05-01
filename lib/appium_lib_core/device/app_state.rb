bf53ae8cd6d33389435530e16cd4a0c80d04176f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2018-05-02 01:17:09 +0900

    add freeze and fix rubocop

--- lib/appium_lib_core/device/app_state.rb
+++ lib/appium_lib_core/device/app_state.rb
@@ -3,12 +3,12 @@ module Appium
     module Device
       class AppState
         STATUS = [
-          :not_installed,                  # 0
-          :not_running,                    # 1
-          :running_in_background_suspended,# 2
-          :running_in_background,          # 3
-          :running_in_foreground           # 4
-        ]
+          :not_installed,                   # 0
+          :not_running,                     # 1
+          :running_in_background_suspended, # 2
+          :running_in_background,           # 3
+          :running_in_foreground            # 4
+        ].freeze
       end
     end
   end
