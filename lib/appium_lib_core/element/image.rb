7552c1e1382113ebf5b7a636fce3e89d5df68792
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-06-17 22:13:44 +0900

    add docstring

--- lib/appium_lib_core/element/image.rb
+++ lib/appium_lib_core/element/image.rb
@@ -1,10 +1,12 @@
 module Appium
   module Core
+    #
+    # ImageElement is an element for images by `find_element/s_by_image`
+    #
     class ImageElement
       Point     = Struct.new(:x, :y)
       Dimension = Struct.new(:width, :height)
       Rectangle = Struct.new(:x, :y, :width, :height)
-      Location  = Struct.new(:latitude, :longitude, :altitude)
 
       # Base64ed format
       # @example
@@ -17,33 +19,88 @@ module Appium
         @driver = driver
         @visual = visual
 
-        @data = {}
-        @data[:center_x] = x + width / 2
-        @data[:center_y] = y + height / 2
-        @data[:x] = x
-        @data[:y] = y
-        @data[:width] = width
-        @data[:height] = height
+        @center_x = x + width / 2
+        @center_y = y + height / 2
+        @x = x
+        @y = y
+        @width = width
+        @height = height
       end
 
+      #
+      # Click this element.
+      #
+      # @example
+      #
+      #     e = @@driver.find_element_by_image './test/functional/data/test_element_image.png'
+      #     e.click
+      #
       def click
-        TouchAction.new(@driver).tap(x: @data[:center_x], y: @data[:center_y]).perform
+        TouchAction.new(@driver).tap(x: @center_x, y: @center_y).perform
       end
 
+      #
+      # Get the location of this element.
+      #
+      # @return [Point]
+      #
+      # @example
+      #
+      #     e = @@driver.find_element_by_image './test/functional/data/test_element_image.png'
+      #     assert_equal [39, 1014], [e.location.x, e.location.y]
+      #
       def location
-        Point.new @data[:x], @data[:y]
+        Point.new @x, @y
       end
 
+      #
+      # Get the size of this element
+      #
+      # @return [Dimension]
+      #
+      # @example
+      #
+      #     e = @@driver.find_element_by_image './test/functional/data/test_element_image.png'
+      #         assert_equal [326, 62], [e.size.width, e.size.height]
+      #
       def size
-        Dimension.new @data[:width], @data[:height]
+        Dimension.new @width, @height
       end
 
+      #
+      # Get the dimensions and coordinates of this element.
+      #
+      # @return [Rectangle]
+      #
+      # @example
+      #
+      #     e = @@driver.find_element_by_image './test/functional/data/test_element_image.png'
+      #         assert_equal([39, 1014, 326, 62], [e.rect.x, e.rect.y, e.rect.width, e.rect.height])
+      #
       def rect
-        Rectangle.new @data[:x], @data[:y], @data[:width], @data[:height]
+        Rectangle.new @x, @y, @width, @height
       end
 
       def displayed?
         true
+      end
+
+      #-------------------------------- sugar  --------------------------------
+
+      def first(full_image:, partial_image:, match_threshold: nil, visualize: false)
+        @driver.find_element_by_image(driver: @driver,
+                                      full_image: full_image,
+                                      partial_image: partial_image,
+                                      match_threshold: match_threshold,
+                                      visualize: visualize)
+      end
+
+      def all(full_image:, partial_image:, match_threshold: nil, visualize: false)
+        @driver.find_elements_by_image(driver: @driver,
+                                       full_image: full_image,
+                                       partial_image: partial_image,
+                                       match_threshold: match_threshold,
+                                       visualize: visualize)
       end
     end
   end
