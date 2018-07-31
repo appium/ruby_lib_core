87b97f09d8ed4add762fd6081fa8a6831cb90442
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-07-31 21:46:27 +0900

    gather search contexts

--- lib/appium_lib_core/android_espresso.rb
+++ lib/appium_lib_core/android_espresso.rb
@@ -1,4 +1,3 @@
 # loaded in common/driver.rb
-require_relative 'android/search_context'
 require_relative 'android/device'
 require_relative 'android/espresso/bridge'
--- test/unit/android/device/mjsonwp/definition_test.rb
+++ test/unit/android/device/mjsonwp/definition_test.rb
@@ -80,10 +80,6 @@ class AppiumLibCoreTest
                                                 :get_clipboard,
                                                 :set_clipboard])
           end
-
-          def test_search_context_in_element_class_for_android
-            assert_equal '-android uiautomator', ::Selenium::WebDriver::Element::FINDERS[:uiautomator]
-          end
         end
       end # module MJSONWP
     end # module Device
