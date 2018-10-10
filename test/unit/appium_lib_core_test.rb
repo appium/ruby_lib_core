176acc23611086bb074b8c3b59e92f25237d3981
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-10-10 09:32:32 +0900

    add argumenterror

--- test/unit/appium_lib_core_test.rb
+++ test/unit/appium_lib_core_test.rb
@@ -3,17 +3,25 @@ require 'test_helper'
 class AppiumLibCoreTest
   class RootTest < Minitest::Test
     def test_version
-      assert ::Appium::Core::VERSION != nil
+      assert !::Appium::Core::VERSION.nil?
     end
 
     def test_symbolize_keys
-      result = ::Appium.symbolize_keys({'a' => 1, b: 2})
-      assert_equal({a: 1, b: 2}, result)
+      result = ::Appium.symbolize_keys({ 'a' => 1, b: 2 })
+      assert_equal({ a: 1, b: 2 }, result)
     end
 
     def test_symbolize_keys_nested
-      result = ::Appium.symbolize_keys({'a' => 1, b: { 'c' => 2, d: 3 }})
-      assert_equal({a: 1, b: { c: 2, d: 3 }}, result)
+      result = ::Appium.symbolize_keys({ 'a' => 1, b: { 'c' => 2, d: 3 } })
+      assert_equal({ a: 1, b: { c: 2, d: 3 } }, result)
+    end
+
+    def test_symbolize_keys_raise_argument_error
+      e = assert_raises ArgumentError do
+        ::Appium.symbolize_keys('no hash value')
+      end
+
+      assert_equal 'symbolize_keys requires a hash', e.message
     end
   end
 end
