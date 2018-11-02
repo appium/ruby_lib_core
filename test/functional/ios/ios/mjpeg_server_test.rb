2bfc6b53034ab5076fdea3dd67fb01445ab8d7ed
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-11-02 23:55:04 +0900

    add MjpegServerTest

---
+++ test/functional/ios/ios/mjpeg_server_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+# $ rake test:func:ios TEST=test/functional/ios/ios/mjpeg_server_test.rb
+# rubocop:disable Style/ClassVars
+class AppiumLibCoreTest
+  module Ios
+    class MjpegServerTest < AppiumLibCoreTest::Function::TestCase
+      def setup
+        @@core ||= ::Appium::Core.for(Caps.ios)
+        @@driver ||= @@core.start_driver
+      end
+
+      def teardown
+        save_reports(@@driver)
+      end
+
+      # Can view via http://localhost:9100 by default
+      def test_config
+        @@driver.update_settings({ mjpegServerScreenshotQuality: 10, mjpegServerFramerate: 1})
+        @@driver.update_settings({ mjpegServerScreenshotQuality: 0, mjpegServerFramerate: -100})
+        @@driver.update_settings({ mjpegServerScreenshotQuality: -10, mjpegServerFramerate: 60})
+        @@driver.update_settings({ mjpegServerScreenshotQuality: 100, mjpegServerFramerate: 60})
+      end
+    end
+  end
+end
