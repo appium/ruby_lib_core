4220ec384cff307fe49693303829ff216bcbab2b
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-11-10 00:06:56 +0900

    fix mobile command tests

--- test/functional/ios/ios/mobile_commands_test.rb
+++ test/functional/ios/ios/mobile_commands_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 # $ rake test:func:ios TEST=test/functional/ios/ios/mobile_commands_test.rb
-# rubocop:disable Style/ClassVars
 class AppiumLibCoreTest
   class DriverTest < AppiumLibCoreTest::Function::TestCase
-    def setup
-      @@core ||= ::Appium::Core.for(Caps.ios)
-      @@driver ||= @@core.start_driver
-    end
-
     def teardown
       save_reports(@driver)
     end
@@ -16,33 +10,28 @@ class AppiumLibCoreTest
     # @since Appium 1.9.2
     # Requires simulator
     def test_permission
-      require 'pry' # Will remove after finishing creating a test scenario
-      binding.pry
+      caps = Caps.ios.dup
+      caps[:caps][:permissions] = '{"com.example.apple-samplecode.UICatalog": { "calendar": "YES", "photos": "no" }}'
+      core = ::Appium::Core.for(caps)
+      @driver = core.start_driver
 
-      @@driver.execute_script('mobile: setPermission', {service: 'calendar', state: 'unset', bundleId: 'com.example.apple-samplecode.UICatalog'})
-# open settings => go to calendar => No UICatalog
-      assert @@driver.execute_script('mobile: getPermission', {service: 'calendar',bundleId: 'com.example.apple-samplecode.UICatalog'}) == 'unset'
+      assert @driver.execute_script('mobile: getPermission',
+                                    { service: 'calendar', bundleId: 'com.example.apple-samplecode.UICatalog' }) == 'yes'
+      assert @driver.execute_script('mobile: getPermission',
+                                    { service: 'photos', bundleId: 'com.example.apple-samplecode.UICatalog' }) == 'no'
 
-      @@driver.execute_script('mobile: setPermission', {service: 'calendar', state: 'yes', bundleId: 'com.example.apple-samplecode.UICatalog'})
-      # open settings => go to calendar => Exist UICatalog
-      assert @@driver.execute_script('mobile: getPermission', {service: 'calendar',bundleId: 'com.example.apple-samplecode.UICatalog'}) == 'yes'
+      @driver.activate_app('com.apple.Preferences')
+      @driver.find_element(:accessibility_id, 'Privacy').click
 
-      @@driver.activate_app('com.apple.settings')
+      @driver.find_element(:accessibility_id, 'Calendars').click
+      el = @driver.find_element(:accessibility_id, 'UICatalog')
+      assert_equal '1', el.value
 
-      @@driver.execute_script('mobile: setPermission', {service: 'calendar', state: 'no', bundleId: 'com.example.apple-samplecode.UICatalog'})
-      # open settings => go to calendar => No UICatalog
-      assert @@driver.execute_script('mobile: getPermission', {service: 'calendar',bundleId: 'com.example.apple-samplecode.UICatalog'}) == 'no'
+      @driver.back
 
-      # Privacy => Calendars => UICatalog's switch
-
-      # Note: Once we change the status, we never back to "unset"
-      @@driver.execute_script('mobile: setPermission', {service: 'calendar', state: 'unset', bundleId: 'com.example.apple-samplecode.UICatalog'})
-
-      assert @@driver.switch_to.alert.text.include?('Allow “Calendar” to access your location while you are using the app?')
-
-      # applesimutils --byId 3CB9E12B-419C-49B1-855A-45322861F1F7  --bundle com.apple.mobilecal --setPermissions calendar=yes
+      @driver.find_element(:accessibility_id, 'Photos').click
+      el = @driver.find_element(:accessibility_id, 'Never')
+      assert_equal 'Never', el.value
     end
-
   end
 end
-# rubocop:enable Style/ClassVars
