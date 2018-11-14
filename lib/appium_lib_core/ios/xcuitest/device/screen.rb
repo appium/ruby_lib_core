995cd5b2b27ef9a52a877835c6cfa5cf79e24183
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2018-11-14 12:35:23 +0900

    sync video_fps (#162)

--- lib/appium_lib_core/ios/xcuitest/device/screen.rb
+++ lib/appium_lib_core/ios/xcuitest/device/screen.rb
@@ -8,7 +8,7 @@ module Appium
               ::Appium::Core::Device.add_endpoint_method(:start_recording_screen) do
                 # rubocop:disable Metrics/ParameterLists
                 def start_recording_screen(remote_path: nil, user: nil, pass: nil, method: nil, force_restart: nil,
-                                           video_type: 'mp4', time_limit: '180', video_quality: 'medium')
+                                           video_type: 'mp4', time_limit: '180', video_quality: 'medium', video_fps: nil)
                   option = ::Appium::Core::Base::Device::ScreenRecord.new(
                     remote_path: remote_path, user: user, pass: pass, method: method, force_restart: force_restart
                   ).upload_option
@@ -16,6 +16,7 @@ module Appium
                   option[:videoType] = video_type
                   option[:timeLimit] = time_limit
                   option[:videoQuality] = video_quality
+                  option[:videoFps] = video_fps unless video_fps.nil?
 
                   execute(:start_recording_screen, {}, { options: option })
                 end
--- test/unit/ios/device/w3c/commands_test.rb
+++ test/unit/ios/device/w3c/commands_test.rb
@@ -44,10 +44,10 @@ class AppiumLibCoreTest
 
           def test_start_recording_screen_custom
             stub_request(:post, "#{SESSION}/appium/start_recording_screen")
-              .with(body: { options: { videoType: 'h265', timeLimit: '60', videoQuality: 'medium' } }.to_json)
+              .with(body: { options: { videoType: 'h265', timeLimit: '60', videoQuality: 'medium', videoFps: '50' } }.to_json)
               .to_return(headers: HEADER, status: 200, body: { value: ['a'] }.to_json)
 
-            @driver.start_recording_screen video_type: 'h265', time_limit: '60'
+            @driver.start_recording_screen video_type: 'h265', time_limit: '60', video_fps: '50'
 
             assert_requested(:post, "#{SESSION}/appium/start_recording_screen", times: 1)
           end
