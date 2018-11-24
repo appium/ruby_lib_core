44a0fff997e1308d3885d710a04d251b0e64211e
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2018-11-24 21:00:21 +0900

    add siri func test for mobile command (#167)
    * add siri func test for mobile command
    
    * tweak siri test
    
    * fix rubocop

--- test/functional/ios/ios/mobile_commands_test.rb
+++ test/functional/ios/ios/mobile_commands_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 # $ rake test:func:ios TEST=test/functional/ios/ios/mobile_commands_test.rb
 class AppiumLibCoreTest
-  class DriverTest < AppiumLibCoreTest::Function::TestCase
+  class MobileCommandsTest < AppiumLibCoreTest::Function::TestCase
+    def setup
+      @core = ::Appium::Core.for(Caps.ios)
+    end
+
     def teardown
       save_reports(@driver)
     end
@@ -32,6 +36,36 @@ class AppiumLibCoreTest
       @driver.find_element(:accessibility_id, 'Photos').click
       el = @driver.find_element(:accessibility_id, 'Never')
       assert_equal 'Never', el.value
+    end
+
+    # @since Appium 1.10.0
+    def test_siri
+      @driver = @core.start_driver
+      assert @driver.app_state('com.example.apple-samplecode.UICatalog') == :running_in_foreground
+      siri_state = @driver.app_state('com.apple.SiriViewService')
+      assert [:running_in_background_suspended, :not_running].include? siri_state
+
+      @driver.execute_script 'mobile: activateSiri', { text: 'hello, siri' }
+
+      e = @driver.find_element :accessibility_id, 'hello, siri'
+      assert_equal 'hello, siri', e.text
+
+      assert_equal :running_in_foreground, @driver.app_state('com.example.apple-samplecode.UICatalog')
+      assert @driver.app_state('com.apple.SiriViewService') == :running_in_background
+
+      @driver.activate_app 'com.example.apple-samplecode.UICatalog'
+      sleep 1 # wait a bit for switching siri service with the test target app
+      assert_equal :running_in_background_suspended, @driver.app_state('com.apple.SiriViewService')
+    end
+
+    def test_source
+      @driver = @core.start_driver
+
+      json = @driver.execute_script 'mobile: source', { format: :json }
+      xml = @driver.execute_script 'mobile: source', { format: :xml }
+
+      assert !json.empty?
+      assert !xml.empty?
     end
   end
 end
