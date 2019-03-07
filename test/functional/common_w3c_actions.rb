90c57cd20013a8d94a479cf8066ff68c2aeef4fe
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-03-07 13:18:03 +0900

    Add w3c common actions scroll (#197)
    * add w3c actions for both android and ios
    
    * add a new line
    
    * update readme

---
+++ test/functional/common_w3c_actions.rb
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Scroll action for Android and iOS following W3C spec
+def w3c_scroll(driver)
+  window = driver.window_rect
+
+  action_builder = driver.action
+  input = action_builder.pointer_inputs[0]
+  action_builder
+    .move_to_location(window.width / 2, window.width * 8 / 10)
+    .pointer_down(:left)
+    .pause(input, 1)
+    .move_to_location(window.width / 2, window.width / 10)
+    .pause(input, 1)
+    .release
+    .perform
+end
--- test/functional/ios/webdriver/w3c_actions_test.rb
+++ test/functional/ios/webdriver/w3c_actions_test.rb
@@ -1,4 +1,17 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'test_helper'
+require 'functional/common_w3c_actions'
 
 # $ rake test:func:ios TEST=test/functional/ios/webdriver/w3c_actions_test.rb
 # rubocop:disable Style/ClassVars
@@ -26,6 +39,8 @@ class AppiumLibCoreTest
       end
 
       def test_scroll
+        skip if @@driver.dialect == :oss
+
         el = @@core.wait { @@driver.find_element(:accessibility_id, 'Controls') }
         @@driver.action.click(el).perform
 
@@ -38,6 +53,21 @@ class AppiumLibCoreTest
 
           @@driver.execute_script('mobile: scroll', direction: 'down')
         end
+      end
+
+      def test_scroll2
+        skip if @@driver.dialect == :oss
+
+        el = @@core.wait { @@driver.find_element(:accessibility_id, 'Controls') }
+        @@driver.action.click(el).perform
+
+        el = @@core.wait { @@driver.find_element(:accessibility_id, 'Style Gray') }
+        assert el.visible == 'false'
+
+        w3c_scroll @@driver
+
+        el = @@core.wait { @@driver.find_element(:accessibility_id, 'Style Gray') }
+        assert el.visible == 'true'
       end
     end
   end
