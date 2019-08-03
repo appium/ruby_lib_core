b2219f79398c4da200d8bc6a9b2f334277b88d14
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-03 15:42:43 +0900

    relax timeout for ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -224,13 +224,13 @@ class AppiumLibCoreTest
           systemPort: system_port,
           language: 'en',
           locale: 'US',
-          adbExecTimeout: 10_000, # 10 sec
           # An emulator 8.1 has Chrome/61.0.3163.98
           # Download a chrome driver from https://chromedriver.storage.googleapis.com/index.html?path=2.34/
           # chromedriverExecutable: "#{Dir.pwd}/test/functional/app/chromedriver_2.34",
           chromeOptions: {
             args: ['--disable-popup-blocking']
           },
+          adbExecTimeout: 60_000, # ms
           uiautomator2ServerLaunchTimeout: 60_000 # ms
         },
         appium_lib: {
