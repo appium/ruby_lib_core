9004429de9864f65e6ebf9777ff688688801cea3
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-12-20 11:26:37 +0900

    run with iOS 13.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -99,7 +99,7 @@ class AppiumLibCoreTest
 
     # Require a simulator which OS version is 11.4, for example.
     def ios(platform_name = :ios)
-      platform_version = '13.0'
+      platform_version = '13.3'
       wda_port = wda_local_port
 
       real_device = ENV['REAL'] ? true : false
