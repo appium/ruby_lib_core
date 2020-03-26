d2b682a3e5229943e45af74bbaf20b0583a64605
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-03-26 10:38:05 +0900

    ci: bump to Xcode 11.4 and iOS 13.4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -103,7 +103,7 @@ class AppiumLibCoreTest
 
     # Require a simulator which OS version is 11.4, for example.
     def ios(platform_name = :ios)
-      platform_version = '13.3'
+      platform_version = '13.4'
       wda_port = wda_local_port
 
       real_device = ENV['REAL'] ? true : false
