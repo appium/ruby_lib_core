bc78cebf383d22c6fa63dd233083f827d6e9be5b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-06-15 02:01:09 +0900

    revert

--- test/functional/android/android/device_test.rb
+++ test/functional/android/android/device_test.rb
@@ -106,6 +106,27 @@ class AppiumLibCoreTest
         assert Date.parse(@driver.device_time('YYYY-MM-DD')).is_a?(Date)
       end
 
+      def test_context_related
+        @@core.wait { scroll_to('Views') }.click
+        @@core.wait { scroll_to('WebView') }.click
+
+        @@core.wait { assert_equal 'NATIVE_APP', @driver.current_context }
+
+        native_page = @driver.page_source
+
+        contexts = @driver.available_contexts
+        webview_context = contexts.detect { |e| e.start_with?('WEBVIEW') }
+
+        @driver.set_context webview_context
+        @@core.wait { assert @driver.current_context.start_with? 'WEBVIEW' }
+
+        webview_page = @driver.page_source
+
+        @driver.switch_to_default_context
+        @@core.wait { assert_equal 'NATIVE_APP', @driver.current_context }
+        assert native_page != webview_page
+      end
+
       def test_app_string
         assert @driver.app_strings.key? 'activity_save_restore'
       end
@@ -155,6 +176,203 @@ class AppiumLibCoreTest
       def test_current_package
         e = @@core.wait { @driver.current_package }
         assert_equal 'io.appium.android.apis', e
+      end
+
+      def test_touch_actions
+        Appium::Core::TouchAction.new(@driver)
+                                 .press(element: @driver.find_element(:accessibility_id, 'App'))
+                                 .release
+                                 .perform
+
+        @@core.wait { @driver.find_element :accessibility_id, 'Action Bar' }
+        @driver.back
+      end
+
+      def test_swipe
+        @@core.wait { @driver.find_element :accessibility_id, 'App' }.click
+
+        el = @@core.wait { @driver.find_element :accessibility_id, 'Fragment' }
+        rect = el.rect
+
+        Appium::Core::TouchAction.new(@driver)
+                                 .swipe(start_x: 75, start_y: 500, end_x: 75, end_y: 500, duration: 500)
+                                 .perform
+        @driver.back # The above command become "tap" action since it doesn't move.
+        el = @@core.wait { @driver.find_element :accessibility_id, 'Fragment' }
+        assert rect.x == el.rect.x
+        assert rect.y == el.rect.y
+
+        touch_action = Appium::Core::TouchAction.new(@driver)
+                                                .swipe(start_x: 75, start_y: 500, end_x: 75, end_y: 300, duration: 500)
+
+        assert_equal :press, touch_action.actions[0][:action]
+        assert_equal({ x: 75, y: 500 }, touch_action.actions[0][:options])
+
+        assert_equal :wait, touch_action.actions[1][:action]
+        assert_equal({ ms: 500 }, touch_action.actions[1][:options])
+
+        assert_equal :moveTo, touch_action.actions[2][:action]
+        assert_equal({ x: 75, y: 300 }, touch_action.actions[2][:options])
+
+        assert_equal :release, touch_action.actions[3][:action]
+
+        touch_action.perform
+        assert_equal [], touch_action.actions
+
+        assert rect.x == el.rect.x
+        assert rect.y > el.rect.y
+      end
+
+      def test_hidekeyboard
+        @@core.wait { @driver.find_element :accessibility_id, 'App' }.click
+        @@core.wait { @driver.find_element :accessibility_id, 'Activity' }.click
+        @@core.wait { @driver.find_element :accessibility_id, 'Custom Title' }.click
+        # make sure to show keyboard
+        @@core.wait { @driver.find_element :id, 'io.appium.android.apis:id/left_text_edit' }.click
+
+        latin_android = 'com.android.inputmethod.latin/.LatinIME'
+        latin_google = 'com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME'
+        imes = @driver.ime_available_engines
+        ime_latin = if imes.member? latin_android # Android O-
+                      latin_android
+                    elsif imes.member? latin_google # Android O+
+                      latin_google
+                    else
+                      imes.first
+                    end
+        @driver.ime_activate ime_latin
+
+        assert @driver.keyboard_shown?
+
+        @@core.wait { @driver.hide_keyboard }
+        sleep 1 # wait animation
+
+        assert !@driver.is_keyboard_shown
+      end
+
+      def test_get_system_bars
+        assert @driver.get_system_bars
+      end
+
+      def test_get_display_density
+        assert(@driver.get_display_density > 0)
+      end
+
+      def test_keyevent
+        skip('Because only for Selendroid')
+        # http://developer.android.com/reference/android/view/KeyEvent.html
+        assert @driver.keyevent(176)
+      end
+
+      def test_press_keycode
+        # http://developer.android.com/reference/android/view/KeyEvent.html
+        result = @driver.press_keycode(176) # it does not raise error
+        assert result || result.nil?
+      end
+
+      def test_long_press_keycode
+        # http://developer.android.com/reference/android/view/KeyEvent.html
+        result = @driver.long_press_keycode(176) # it does not raise error
+        assert result || result.nil?
+      end
+
+      def test_open_notifications
+        skip if @@core.automation_name == :espresso
+
+        # test & comments from https://github.com/appium/appium/blob/master/test/functional/android/apidemos/notifications-specs.js#L19
+        # get to the notification page
+        @@core.wait { scroll_to('App').click }
+        @@core.wait { scroll_to('Notification').click }
+        @@core.wait { scroll_to('Status Bar').click }
+        # create a notification
+        @@core.wait { @driver.find_element :accessibility_id, ':-|' }.click
+        @driver.open_notifications
+        # shouldn't see the elements behind shade
+
+        # Sometimes, we should wait animation
+        @@core.wait(timeout: 5) do
+          error = assert_raises ::Selenium::WebDriver::Error::WebDriverError do
+            @driver.find_element :accessibility_id, ':-|'
+          end
+          assert 'An element could not be located on the page using the given search parameters.', error.message
+        end
+
+        # should see the notification
+        @@core.wait_true { text 'Mood ring' }
+        # return to app
+        @driver.back
+        # should be able to see elements from app
+        @@core.wait_true { @driver.find_element :accessibility_id, ':-|' }
+      end
+
+      def test_ime_available_engins
+        @@core.wait { @driver.find_element :accessibility_id, 'App' }.click
+        @@core.wait { @driver.find_element :accessibility_id, 'Activity' }.click
+        @@core.wait { @driver.find_element :accessibility_id, 'Custom Title' }.click
+
+        imes = @driver.ime_available_engines
+        assert imes.length > 1
+
+        available_ime = imes.first
+        @@core.wait { @driver.ime_activate(available_ime) }
+        assert_equal available_ime, @driver.ime_active_engine
+        assert @driver.ime_activated
+
+        @@core.wait { @driver.ime_deactivate }
+        refute_equal available_ime, @driver.ime_active_engine
+
+        @@core.wait do
+          @driver.ime_activate(available_ime)
+          assert_equal available_ime, @driver.ime_active_engine
+        end
+      end
+
+      def test_within_context
+        result = @driver.within_context('NATIVE_APP') do
+          @@core.wait { @driver.find_element :accessibility_id, 'App' }
+        end
+
+        assert_equal 'App', result.text
+      end
+
+      private
+
+      def scroll_to(text)
+        if @@core.automation_name == :espresso
+          @driver.find_element :accessibility_id, text
+        else
+          text = %("#{text}")
+          rid  = resource_id(text, "new UiSelector().resourceId(#{text});")
+          args = if rid.empty?
+                   ["new UiSelector().textContains(#{text})", "new UiSelector().descriptionContains(#{text})"]
+                 else
+                   [rid]
+                 end
+          args.each_with_index do |arg, index|
+            begin
+              elem = @driver.find_element :uiautomator,
+                                          'new UiScrollable(new UiSelector().scrollable(true).instance(0))' \
+                                          ".scrollIntoView(#{arg}.instance(0));"
+              return elem
+            rescue StandardError => e
+              raise e if index == args.size - 1
+            end
+          end
+        end
+      end
+
+      def resource_id(string, on_match)
+        return '' unless string
+
+        unquote = string.match(/"(.+)"/)
+        string = unquote[1] if unquote
+
+        resource_id = %r{^[a-zA-Z_][a-zA-Z0-9\._]*:[^\/]+\/[\S]+$}
+        string.match(resource_id) ? on_match : ''
+      end
+
+      def text(value)
+        @driver.find_element :uiautomator, "new UiSelector().className(\"android.widget.TextView\").text(\"#{value}\");"
       end
     end
   end
