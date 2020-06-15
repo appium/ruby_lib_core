cf546a0c62f815df4bb3ece4932b8033fad25ae2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-06-15 10:28:17 +0900

    ci: tune Azure (#269)
    * ci: tune Azure preferences

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -270,9 +270,13 @@ class AppiumLibCoreTest
         }
       }
 
+      cap[:desired_capabilities][:buildToolsVersion] = ENV['ANDROID_BUILD_TOOLS_VERSION'] if ENV['ANDROID_BUILD_TOOLS_VERSION']
+
       # settins in caps should work over Appium 1.13.0
       if cap[:desired_capabilities][:automationName] == 'uiautomator2' && AppiumLibCoreTest.appium_version == 'beta'
         cap[:desired_capabilities]['settings[trackScrollEvents]'] = false
+      else
+        cap[:desired_capabilities][:forceEspressoRebuild] = false
       end
 
       cap
