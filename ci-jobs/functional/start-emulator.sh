d3641c085bdba4138f4d4baa068cdcab7756d5ce
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-07-29 15:18:05 +0900

    show list avd

--- ci-jobs/functional/start-emulator.sh
+++ ci-jobs/functional/start-emulator.sh
@@ -10,16 +10,13 @@ ${ANDROID_HOME}/tools/bin/sdkmanager --list
 declare -r emulator="system-images;android-${ANDROID_SDK_VERSION};google_apis;x86"
 echo "y" | ${ANDROID_HOME}/tools/bin/sdkmanager --install "$emulator"
 
-echo "y" | ${ANDROID_HOME}/tools/bin/sdkmanager --install "$emulator"
-
-
 # Show a list of emulators
 ${ANDROID_HOME}/tools/bin/avdmanager list
 
 # Create emulator
 echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd -d "Nexus 5X" -n testemulator -k "${emulator}" --force
 
-echo ${ANDROID_HOME}/emulator/emulator -list-avds
+${ANDROID_HOME}/tools/bin/avdmanager list avd
 
 echo "Starting emulator"
 
