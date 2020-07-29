ef7181e1b925899e008722aacd13398b258006cf
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-07-29 16:14:03 +0900

    remove > /dev/null for debug

--- ci-jobs/functional/start-emulator.sh
+++ ci-jobs/functional/start-emulator.sh
@@ -21,7 +21,7 @@ ${ANDROID_HOME}/tools/bin/avdmanager list avd
 echo "Starting emulator"
 
 # Start emulator in background
-nohup ${ANDROID_HOME}/emulator/emulator -avd testemulator -accel auto -no-boot-anim -no-snapshot > /dev/null 2>&1 &
+nohup ${ANDROID_HOME}/emulator/emulator -avd testemulator -accel auto -no-boot-anim -no-snapshot 2>&1 &
 ${ANDROID_HOME}/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 ${ANDROID_HOME}/platform-tools/adb devices
