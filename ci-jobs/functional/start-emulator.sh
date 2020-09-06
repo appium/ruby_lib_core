42975f1f318c2c99c58d57b140da16cb634fee82
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-09-07 00:32:48 +0900

    update

--- ci-jobs/functional/start-emulator.sh
+++ ci-jobs/functional/start-emulator.sh
@@ -17,9 +17,16 @@ echo ${ANDROID_HOME}/emulator/emulator -list-avds
 
 echo "Starting emulator"
 
-# Start emulator in background
-nohup ${ANDROID_HOME}/emulator/emulator -avd testemulator -no-audi -accel auto -gpu auto -no-boot-anim -no-snapshot -delay-adb > /dev/null 2>&1 &
-${ANDROID_HOME}/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+if [ $ANDROID_SDK_VERSION -ge 28 ]; then
+  nohup $ANDROID_HOME/emulator/emulator -avd $ANDROID_AVD -no-audio -accel auto -gpu auto -no-boot-anim -no-snapshot -delay-adb > /dev/null 2>&1 &
+  $ANDROID_HOME/platform-tools/adb wait-for-device || exit 1
+  bootDuration=$(( $(date +%s) - secondsStarted ))
+  echo "Emulator booting took ${bootDuration}s"
+  adb shell input keyevent 82
+else
+  nohup $ANDROID_HOME/emulator/emulator -avd $ANDROID_AVD -no-audio -accel auto -gpu auto -no-boot-anim -no-snapshot > /dev/null 2>&1 &
+  ${ANDROID_HOME}/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+fi
 
 ${ANDROID_HOME}/platform-tools/adb devices
 
