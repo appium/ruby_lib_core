8d9d20770dcc4ed8f08552375bdb673af719e812
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-11-23 16:43:36 +0900

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -125,6 +125,7 @@ class AppiumLibCoreTest
           someCapability: 'some_capability',
           newCommandTimeout: 120,
           wdaLocalPort: wda_port,
+          showXcodeLog: true,
           # `true`, which is the default value, is faster to finishing launching part in many cases
           # But sometimes `false` is necessary. It leads regressions sometimes though.
           waitForQuiescence: true,
