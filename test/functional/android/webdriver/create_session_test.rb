e27a4e29d39036754abd35d6e5071a7ff987fb5d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-03-01 01:32:51 -0800

    remove mjsonwp

--- test/functional/android/webdriver/create_session_test.rb
+++ test/functional/android/webdriver/create_session_test.rb
@@ -18,36 +18,6 @@ require 'test_helper'
 class AppiumLibCoreTest
   module WebDriver
     class CreateSessionTestTest < AppiumLibCoreTest::Function::TestCase
-      def test_mjsonwp
-        caps = Caps.android[:desired_capabilities].merge({ forceMjsonwp: true })
-        new_caps = Caps.android.merge({ caps: caps })
-        core = ::Appium::Core.for(new_caps)
-
-        driver = core.start_driver
-
-        assert_equal :w3c, driver.dialect
-        assert driver.capabilities[:forceMjsonwp].nil?
-        assert driver.capabilities['forceMjsonwp'].nil?
-
-        core.quit_driver
-      end
-
-      def test_w3c
-        skip_as_appium_version '1.8.0'
-
-        caps = Caps.android[:desired_capabilities].merge({ forceMjsonwp: false })
-        new_caps = Caps.android.merge({ caps: caps })
-        core = ::Appium::Core.for(new_caps)
-
-        driver = core.start_driver
-
-        assert_equal :w3c, driver.dialect
-        assert driver.capabilities[:forceMjsonwp].nil?
-        assert driver.capabilities['forceMjsonwp'].nil?
-
-        driver.quit # We can quit driver in this way as well
-      end
-
       def test_w3c_default
         skip_as_appium_version '1.8.0'
 
