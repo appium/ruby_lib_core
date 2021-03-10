a0a3cfc71783bed3d1b0e7afbf6bc0a27bf60a48
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-03-11 04:15:31 +0900

    feat: add speed option (#318)
    * add speed option
    
    * tweak docstring
    
    * use android sdk 29 to make the sumulator stable
    
    * add note

---
+++ lib/appium_lib_core/common/base/has_location.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Appium
+  module Core
+    class Base
+      #
+      # @api private
+      #
+      module HasLocation
+        # Get the location of the device.
+        #
+        # @return [::Selenium::WebDriver::Location]
+        #
+        # @example
+        #
+        #   driver.location #=> ::Selenium::WebDriver::Location.new(10, 10, 10)
+        #
+        def location
+          @bridge.location
+        end
+
+        # Set the location of the device.
+        #
+        # @param [::Selenium::WebDriver::Location] location Set the location.
+        #
+        # @example
+        #
+        #   driver.location = ::Selenium::WebDriver::Location.new(10, 10, 10)
+        #
+        def location=(location)
+          unless location.is_a?(::Selenium::WebDriver::Location)
+            raise TypeError, "expected #{::Selenium::WebDriver::Location}, got #{location.inspect}:#{location.class}"
+          end
+
+          @bridge.set_location location.latitude, location.longitude, location.altitude
+        end
+
+        # Set the location of the device.
+        #
+        # @param [String, Number] latitude Set the latitude.
+        # @param [String, Number] longitude Set the longitude.
+        # @param [String, Number] altitude Set the altitude.
+        # @param [String, Number] speed Set the speed to apply the location on Android real devices @since Appium 1.21.0.
+        # @param [::Selenium::WebDriver::Location]
+        #
+        # @example
+        #
+        #   driver.location = ::Selenium::WebDriver::Location.new(10, 10, 10)
+        #
+        def set_location(latitude, longitude, altitude, speed: nil)
+          if speed.nil?
+            self.location = ::Selenium::WebDriver::Location.new(Float(latitude), Float(longitude), Float(altitude))
+          else
+            loc = ::Selenium::WebDriver::Location.new(Float(latitude), Float(longitude), Float(altitude))
+            @bridge.set_location loc.latitude, loc.longitude, loc.altitude, speed: Float(speed)
+          end
+        end
+      end
+    end
+  end
+end
