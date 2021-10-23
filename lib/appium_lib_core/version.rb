1da3e753907ac431d925a1489a50af3fa406638f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-10-23 00:59:23 -0700

    chore: bump to rc6

--- lib/appium_lib_core/version.rb
+++ lib/appium_lib_core/version.rb
@@ -14,7 +14,7 @@
 
 module Appium
   module Core
-    VERSION = '5.0.0.rc5' unless defined? ::Appium::Core::VERSION
-    DATE    = '2021-10-15' unless defined? ::Appium::Core::DATE
+    VERSION = '5.0.0.rc6' unless defined? ::Appium::Core::VERSION
+    DATE    = '2021-10-23' unless defined? ::Appium::Core::DATE
   end
 end
