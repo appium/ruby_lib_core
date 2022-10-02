bcbb737ab221b479a11d3116cb60e2ff71555955
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-10-02 09:57:03 -0700

    test: add error message

--- test/functional/ios/driver_test.rb
+++ test/functional/ios/driver_test.rb
@@ -124,8 +124,8 @@ class AppiumLibCoreTest
       ensure
         @@driver.activate_app(bundle_id)
       end
-      assert_equal '0', switches_status['Auto-Correction']
-      assert_equal '0', switches_status['Predictive']
+      assert_equal '0', switches_status['Auto-Correction'], "Switches are #{switches_status}"
+      assert_equal '0', switches_status['Predictive']], "Switches are #{switches_status}"
     end
 
     # @since Appium 1.15.0
