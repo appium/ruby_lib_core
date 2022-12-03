162027b129efea6189efea27160aacaf59f25266
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-12-03 00:31:30 -0800

    tweak keyboards

--- test/functional/ios/driver_test.rb
+++ test/functional/ios/driver_test.rb
@@ -119,16 +119,13 @@ class AppiumLibCoreTest
         # to wait the animation
         @@driver.wait { |d| d.find_element :accessibility_id, 'Auto-Correction' }
 
-        eles = @@driver.find_elements :class, 'XCUIElementTypeSwitch'
-
-        puts @@driver.page_source
-
-        switches_status = eles.each_with_object({}) { |e, acc| acc[e.name] = e.value }
+        auto_correction = @@driver.wait { |d| d.find_element :predict, 'name == "Auto-Correction" AND type == "XCUIElementTypeSwitch"' }
+        predictive = @@driver.wait { |d| d.find_element :predict, 'name == "Predictive" AND type == "XCUIElementTypeSwitch"' }
+        assert_equal '0', auto_correction.value
+        assert_equal '0', predictive.value
       ensure
         @@driver.activate_app(bundle_id)
       end
-      assert_equal '0', switches_status['Auto-Correction'], "Switches are #{switches_status}"
-      assert_equal '0', switches_status['Predictive'], "Switches are #{switches_status}"
     end
 
     # @since Appium 1.15.0
