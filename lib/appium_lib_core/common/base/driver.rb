5b5e47b05e0f8f2577af4f0d595658531f3efc66
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-08-08 18:04:06 +0900

    fix: fix rubocop

--- lib/appium_lib_core/common/base/driver.rb
+++ lib/appium_lib_core/common/base/driver.rb
@@ -81,7 +81,7 @@ module Appium
 
           raise ::Appium::Core::Error::ArgumentError, "Unable to create a driver with parameters: #{opts}" unless opts.empty?
 
-          @has_bidi = !!(capabilities && capabilities['webSocketUrl'])
+          @has_bidi = capabilities && capabilities['webSocketUrl'] ? true : false
           bridge_clzz = @has_bidi ? ::Appium::Core::Base::BiDiBridge : ::Appium::Core::Base::Bridge
           bridge = bridge_clzz.new(**bridge_opts)
 
